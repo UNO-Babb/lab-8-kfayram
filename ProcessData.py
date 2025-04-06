cfbe3db871720708fd5ecb39858cccb3c0d181a3
kfayram <gafferkyle@hotmail.com>
2025-04-06 21:20:42 +0000

    finished id

--- ProcessData.py
+++ ProcessData.py
@@ -1,5 +1,5 @@
 #ProcessData.py
-#Name:
+#Name: Kyle
 #Date:
 #Assignment:
 
@@ -12,12 +12,35 @@ def main():
   outFile = open("StudentList.csv", 'w')
 
   #Process each line of the input file and output to the CSV file
+  
+  #line = inFile.readline()
+  for line in inFile:
+    data = line.split()
+    first = data[0]
+    last = data[1]
+    idNum = data[3]
 
-
+    student_id = makeID(first, last, idNum)
+    output = last + "," + first + "," + student_id + "\n"
+    outFile.write(output)
 
   #Close files in the end to save and ensure they are not damaged.
   inFile.close()
   outFile.close()
+
+def makeID(first, last, idNum):
+  #print(first, last, idNum)
+  idLen = len(idNum)
+
+  while len(last) < 5:
+    last = last + "X"
+
+  id = first[0] + last + idNum[idLen - 3: ]
+
+  #print(id)
+  return(id)
+
+
 
 if __name__ == '__main__':
   main()
